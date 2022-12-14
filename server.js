d9af0e2a9c9a3218797ff15f7c024e83535a1901
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-12-14 15:21:15 +0530

    Create basic server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const dotenv = require("dotenv")
+const colors = require("colors")
+
+dotenv.config()
+
+const app = express()
+
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+	res.send("<h1>Hello from server</h1>")
+})
+
+const PORT = 8080 || process.env.PORT
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
